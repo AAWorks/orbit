40809d5e34233da370481b9cebaeef0139f6dfe5
Alejandro Alonso <66284316+AAWorks@users.noreply.github.com>
2023-03-30 11:08:01 -0500

    authy done

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ import streamlit as st
 import regex as re
 from db_funcs import UserDB
 
+DB_FILE = "pkr.db"
+
 class Auth:
     def __init__(self):
         st.set_page_config(layout="wide")
@@ -11,19 +13,24 @@ class Auth:
                                 at least one uppercase letter, \
                                  one lowercase letter, one number \
                                  and one special character)")
-        self.user_db = UserDB()
+        self.user_db = UserDB(DB_FILE)
 
     def __check_inputs(self):
-        pwd_reqs = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$"
+        pwd_reqs = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])\
+            [A-Za-z\d@$!%*?&]{8,}$"
         return re.match(pwd_reqs, self.pwd) and self.user.isalnum()
 
     def authorize(self):
         if not self.__check_inputs():
             st.error("Username or Password don't meet specifications.")
-        elif self.user_db.auth_user(self.user, self.pwd):
-            # session
-            pass
-
+        else:
+            self.user_db.auth_user(self.user, self.pwd):    
+    
+    def register(self):
+        if not self.__check_inputs():
+            st.error("Username or Password don't meet specifications.")
+        else:
+            self.user_db.add_user(self.user, self.pwd)
 
 auth = Auth()
 st.button("Log In", use_container_width=True, on_click=auth.authorize)
