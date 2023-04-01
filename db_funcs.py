8b10c572508598afb2c932cb737700c8c7ca81d4
Alejandro Alonso <66284316+AAWorks@users.noreply.github.com>
2023-04-01 18:07:28 -0500

    authy complete

--- db_funcs.py
+++ db_funcs.py
@@ -4,18 +4,21 @@ import streamlit as st
 
 ''' Database-related Functions '''
 
-class UserDB:
-    @st.cache_resource
-    def __init__(self, db_file: str):
-        self.__db = SqliteDb(db_file)
-        self.__db.query("CREATE TABLE IF NOT EXISTS users (username TEXT, \
-                password TEXT, userID INTEGER PRIMARY KEY AUTOINCREMENT;") 
-        self.__db.query("CREATE TABLE IF NOT EXISTS ledgers (userID INTEGER, \
-                dt DATETIME DEFAULT CURRENT_DATE, buy_in INTEGER, \
-                buy_out INTEGER;")
+DB_FILE = "pkr.db"
 
-    def get_user_id(self, username: str) -> int:
-        return self.__db.select("users", username=username)[0].userID
+@st.cache_resource
+def db_setup():
+    db = SqliteDb(DB_FILE)
+    db.query("CREATE TABLE IF NOT EXISTS users (username TEXT, \
+                password TEXT);") 
+    db.query("CREATE TABLE IF NOT EXISTS ledgers (user TEXT, \
+            dt DATETIME DEFAULT CURRENT_DATE, buy_in INTEGER, \
+            buy_out INTEGER);")
+    return db
+
+class UserDB:
+    def __init__(self, db):
+        self.__db = db
 
     def __user_exists(self, username: str) -> bool:
         return self.__db.select("users", username=username)
@@ -24,18 +27,17 @@ class UserDB:
         to_check = self.__db.select("users", username=username)[0].password
         return password == to_check # Will add bcrypt protection later
         
-    def auth_user(self, username: str, password: StopAsyncIteration):
+    def auth_user(self, username: str, password: str):
         if not self.__user_exists(username):
             st.error("Username not found")
         elif not self.__check_pwd(username, password):
             st.error("Incorrect Password for Given User")
         else:
-            st.success(f"Welcome {username}!")
-            st.session_state["user_id"] = self.get_user_id(username)
+            st.session_state["username"] = username
 
     def add_user(self, username, password):
         if self.__user_exists(username):
             st.error("Username already exists.")
         else:
             self.__db.insert("users", username=username, password=password)
-            st.session_state["user_id"] = self.get_user_id(username)
+            st.session_state["username"] = username
