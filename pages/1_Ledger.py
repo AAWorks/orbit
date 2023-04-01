396e52b2940fd0c657a99d27e80431e7bfdf84e6
Alejandro Alonso <66284316+AAWorks@users.noreply.github.com>
2023-04-01 18:07:15 -0500

    ledger authy complete

---
+++ pages/1_Ledger.py
@@ -0,0 +1,63 @@
+import streamlit as st
+import regex as re
+from db_funcs import db_setup, UserDB
+
+DB_FILE = "pkr.db"
+
+def streamlit_setup():
+    st.set_page_config(page_title="Orbit", page_icon=":spades:")
+    st.sidebar.success("Interative Poker Ledger")
+    if "username" in st.session_state:
+        st.title(f"{st.session_state['username']}'s Ledger:clubs:")
+    else:
+        st.title('Ledger:clubs:')
+
+class Auth:
+    def __init__(self, db):
+        self.user_db = UserDB(db)
+
+    def __check_inputs(self, user, pwd):
+        pwd_reqs = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$"
+        return re.match(pwd_reqs, pwd) and user.isalnum()
+
+    def authorize(self, user, pwd):
+        if not self.__check_inputs(user, pwd):
+            st.error("Username or Password don't meet specifications.")
+        else:
+            self.user_db.auth_user(user, pwd) 
+    
+    def register(self, user, pwd):
+        if not self.__check_inputs(user, pwd):
+            st.error("Username or Password don't meet specifications.")
+        else:
+            self.user_db.add_user(user, pwd)
+    
+class Ledger:
+    def __init__(self):
+        pass
+
+    def display(self):
+        pass
+
+    def update(self):
+        pass
+
+streamlit_setup()
+db = db_setup()
+if "username" not in st.session_state:
+    with st.form(key='authy'):
+        st.write("Sign In to View Personal Ledger")
+        username = st.text_input("Username (No special characters)")
+        password = st.text_input("Password (8+ characters, \
+                                1+ uppercase letters, \
+                                1+ lowercase letters, 1+ numbers \
+                                and 1+ special character)")
+        login = st.form_submit_button(label='Log In', use_container_width=True)
+        register = st.form_submit_button(label='Register', use_container_width=True)
+    auth = Auth(db)
+    if login:
+        auth.authorize(username, password)
+    elif register:
+        auth.register(username, password)
+else:
+    ledger = Ledger()
