bd31150b05e43d345b18b3cf09f86f9d8107cc15
Alejandro Alonso <66284316+AAWorks@users.noreply.github.com>
2023-04-03 20:20:03 -0500

    minor change - diamonds

--- Variance.py
+++ Variance.py
@@ -5,8 +5,8 @@ from db_funcs import UserDB
 DB_FILE = "pkr.db"
 
 def streamlit_setup():
-    st.set_page_config(layout="wide", page_title="Variance", page_icon=":spades:")
-    st.title('Welcome to Variance:spades:')
+    st.set_page_config(layout="wide", page_title="Variance", page_icon=":diamonds:")
+    st.title('Welcome to Variance:diamonds:')
     st.sidebar.success("Just Keep Gambling.")
 
 streamlit_setup()
